5a04d9e4045253a109de59193d19ddca3cad834f
wooyani77 <wooyani77@gmail.com>
2023-07-11 21:38:55 +0900

    file: 프로젝트 생성

---
+++ ShortFormFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShortFormFeed
+//
+//  Created by Allie on 2023/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
