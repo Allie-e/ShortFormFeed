aead6e206328acb0cfa0595c99ca42ed8073d94b
wooyani77 <wooyani77@gmail.com>
2023-07-15 22:48:18 +0900

    feat: PostCell 구현 #3

---
+++ ShortFormFeed/View/PostCell.swift
@@ -0,0 +1,133 @@
+//
+//  PostCell.swift
+//  ShortFormFeed
+//
+//  Created by Allie on 2023/07/15.
+//
+
+import UIKit
+import SnapKit
+import AVKit
+
+final class PostCell: UICollectionViewCell {
+    static let identifier = "PostCell"
+    
+    private let soundButton: UIButton = {
+        let button = UIButton()
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 30)
+        let image = UIImage(systemName: "speaker.wave.2.fill", withConfiguration: imageConfig)
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        return button
+    }()
+    
+    private let likeButton: UIButton = {
+        let button = UIButton()
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 30)
+        let image = UIImage(systemName: "heart.fill", withConfiguration: imageConfig)
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        return button
+    }()
+    
+    private let likeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .caption1)
+        label.textAlignment = .center
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private let followButton: UIButton = {
+        let button = UIButton()
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 30)
+        let image = UIImage(systemName: "person.crop.circle.badge.plus", withConfiguration: imageConfig)
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        return button
+    }()
+    
+    private let followLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .caption1)
+        label.textAlignment = .center
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private let shareButton: UIButton = {
+        let button = UIButton()
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 30)
+        let image = UIImage(systemName: "ellipsis", withConfiguration: imageConfig)
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        return button
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupCell() {
+        likeLabel.text = "123"
+        followLabel.text = "12K"
+    }
+    
+    private func setupLayout() {
+        [soundButton, likeButton, likeLabel, followButton, followLabel, shareButton].forEach { view in
+            contentView.addSubview(view)
+        }
+        
+        soundButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(30)
+            make.trailing.equalToSuperview().offset(-20)
+            make.width.height.equalTo(35)
+        }
+        
+        likeButton.snp.makeConstraints { make in
+            make.bottom.equalTo(followButton.snp.top).offset(-40)
+            make.width.height.equalTo(35)
+            make.centerX.equalTo(shareButton)
+        }
+        
+        likeLabel.snp.makeConstraints { make in
+            make.top.equalTo(likeButton.snp.bottom)
+            make.trailing.equalTo(likeButton.snp.trailing)
+            make.centerX.equalTo(shareButton)
+        }
+        
+        followButton.snp.makeConstraints { make in
+            make.bottom.equalTo(shareButton.snp.top).offset(-40)
+            make.width.height.equalTo(35)
+            make.centerX.equalTo(shareButton)
+        }
+        
+        followLabel.snp.makeConstraints { make in
+            make.top.equalTo(followButton.snp.bottom)
+            make.trailing.equalTo(followButton.snp.trailing)
+            make.centerX.equalTo(shareButton)
+        }
+        
+        shareButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-50)
+            make.trailing.equalToSuperview().offset(-20)
+            make.width.height.equalTo(35)
+        }
+    }
+}
