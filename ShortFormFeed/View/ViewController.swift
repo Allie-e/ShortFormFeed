fbfea216243fd4a5a28c72d62345e1458682de78
wooyani77 <wooyani77@gmail.com>
2023-07-16 22:39:31 +0900

    feat: viewModel - viewController 연결 및 bind 함수 구현 #3

--- ShortFormFeed/View/ViewController.swift
+++ ShortFormFeed/View/ViewController.swift
@@ -10,37 +10,91 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
+    private typealias DiffableDataSource = UICollectionViewDiffableDataSource<Section, FeedItem>
+    
+    private enum Section {
+        case main
+    }
+    
+    private enum FeedItem: Hashable {
+        case post(Post)
+    }
+    
+    let viewModel = PostViewModel()
+    private let disposeBag: DisposeBag = .init()
+    private var dataSource: DiffableDataSource?
+    private var snapshot = NSDiffableDataSourceSnapshot<Section, FeedItem>()
+    private var nowPage = 0
+
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 5
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         
-        return cv
+        return collectionView
     }()
-    private var nowPage = 0
-    
-    let videoURL = ["https://storage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCollectionView()
+        configureDataSource()
+        bind()
+    }
+    
+    private func bind() {
+        let input = PostViewModel.Input(viewDidLoadObservable: .just(Void()))
+        let output = viewModel.transform(input)
+        
+        output.loadPostObservable
+            .withUnretained(self)
+            .subscribe(onNext: { owner, feed in
+                guard let feed = feed else { return }
+                owner.applySnapshot(with: feed.posts)
+                print(feed)
+            })
+            .disposed(by: disposeBag)
     }
     
     private func setupCollectionView() {
-        
         self.view.addSubview(collectionView)
-        
         collectionView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
-        collectionView.delegate = self
-        collectionView.dataSource = self
         collectionView.decelerationRate = .fast
-        collectionView.register(PostCell.self, forCellWithReuseIdentifier: PostCell.identifier)
+    }
+    
+    private func configureDataSource() {
+        collectionView.register(PostCell.self)
+        
+        dataSource = DiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            switch item {
+            case .post(let post):
+                let cell = collectionView.dequeueReusableCell(PostCell.self, for: indexPath)
+                cell?.setupCell(with: post)
+                
+                return cell
+            }
+        })
+        
+        collectionView.dataSource = dataSource
+    }
+    
+    private func applySnapshot(with posts: [Post]?) {
+        guard let posts = posts else { return }
+        let postItems = posts.map { FeedItem.post($0) }
+        snapshot = NSDiffableDataSourceSnapshot<Section, FeedItem>()
+        
+        snapshot.appendSections([.main])
+        snapshot.appendItems(postItems)
+        
+        dataSource?.apply(snapshot)
     }
     
     private func startLoop() {
-        let _ = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { _ in
+        let _ = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
             self.moveNextPage()
         }
     }
@@ -54,35 +108,5 @@ class ViewController: UIViewController {
         }
         
         collectionView.scrollToItem(at: IndexPath(item: nowPage, section: 0), at: .centeredVertically, animated: true)
-    }
-}
-
-extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        12
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PostCell.identifier, for: indexPath) as? PostCell else { return UICollectionViewCell() }
-        cell.setupCell(videoURL.randomElement()!)
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if let cell = collectionView.cellForItem(at: indexPath) as? PostCell {
-            cell.videoView?.cleanup()
-        }
-    }
-    
-}
-
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        1
     }
 }
